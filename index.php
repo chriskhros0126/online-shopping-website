39f8d59cf92322fa7b084f87f6081df567410309
chriskhros <johnytan72@gmail.com>
2024-08-05 10:19:30 +0800

    Initial Commit

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/main.css">
+    <title>Alpha</title>
+</head>
+<body>
+    <div class="header">
+        <div class="left">Menu</div>
         
-        <script src="" async defer></script>
-    </body>
+        <div class="middle">
+            <a><img src="https://picsum.photos/50" alt="Logo"></a>
+        </div>
+        
+        <div class="right">
+            <a href="#">Profile</a>
+            <a href="#">Cart</a>
+        </div>
+    </div>
+</body>
+<footer>
+
+</footer>
 </html>
